c569dc43bac118abc6efa1f09f03cfc6ccff8e20
Bob Oyier <boboyier92@gmail.com>
2022-11-24 05:25:44 +0300

    "Completed Moringa School's JavaScript Advanced First-class Functions lab"

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+
+// 'madere' is an array I have used to test the codes:
+let madere = ["Njoro", "Otis", "Kama", "Onyi", "Musyoks", "Karanja"];
+
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.slice(0, 2);
+};
+// console.log(returnFirstTwoDrivers(madere));
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
+};
+// console.log(returnLastTwoDrivers(madere));
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+  return function (fare) {
+    return multiplier * fare;
+  };
+}
+
+const fareDoubler = function (fare) {
+  return createFareMultiplier(2)(fare);
+};
+// console.log(createFareMultiplier(2)(300))
+
+const fareTripler = function (fare) {
+  return createFareMultiplier(3)(fare);
+};
+// console.log(createFareMultiplier(3)(300));
+
+function selectDifferentDrivers(drivers, choice) {
+	return choice(drivers)
+}
+// console.log(returnFirstTwoDrivers(madere));
